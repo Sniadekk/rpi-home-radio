2fec443d22335dc59174fb8c18de65d6f53c85e1
Karol Gruszka <wojokarolg9@gmail.com>
2019-04-10 13:39:22 +0200

    script 0.2

--- install.sh
+++ install.sh
@@ -1,28 +1,12 @@
-# Install gcc
-sudo apt-get install make gcc g++
-# Install ffmpeg & other libraries in RaspberryPi by the following steps-->
-sudo apt-get install libmp3lame-dev
-cd /usr/src
-sudo git clone git://git.videolan.org/x264
-cd x264
-sudo ./configure --host=arm-unknown-linux-gnueabi --enable-static --disable-opencl --enable-shared
-sudo make
-sudo make install
-# This installs libmp3lame and x264 libraries
-cd /usr/src
-sudo git clone git://source.ffmpeg.org/ffmpeg.git ffmpeg
-cd ffmpeg
-sudo ./configure --arch=armv7-a --target-os=linux --enable-gpl --enable-libx264 --enable-nonfree --enable-libmp3lame --extra-cflags='-march=armv7-a -mfpu=neon-vfpv4 -mfloat-abi=hard'
-sudo make -j4
-sudo make install
-sudo ldconfig
 # clone needed repositories
 git clone https://github.com/Sniadekk/home-fm-server
-git clone https://github.com/Sniadekk/home-fm-client
-git clone https://github.com/somu1795/fm_transmitter.git
-# install needed dependencies
-cd home-fm-client
-npm install
-# build server
-cd ../home-fm-server
+cd home-fm-server
+curl https://sh.rustup.rs -sSf | sh
 cargo build
+diesel run migration
+git clone https://github.com/Sniadekk/home-fm-client
+cd ../home-fm-client
+npm install
+git clone https://github.com/markondej/fm_transmitter.git
+cd ../hm_transmitter
+make
