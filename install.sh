31746229e4f1361a1c460a59ef7ad0b1573ae937
Karol Gruszka <wojokarolg9@gmail.com>
2019-04-10 14:32:36 +0200

    add client build to avoid heavy work on rpi

--- install.sh
+++ install.sh
@@ -1,12 +1,18 @@
 # clone needed repositories
 git clone https://github.com/Sniadekk/home-fm-server
-git clone https://github.com/Sniadekk/home-fm-client
+# git clone https://github.com/Sniadekk/home-fm-client
+# let's just download minified client app without doing all the heavy work on rpi
 git clone https://github.com/markondej/fm_transmitter.git
 cd home-fm-server
+# install rust
 curl https://sh.rustup.rs -sSf | sh
+# install cargo tools to run migration
+cargo install diesel_cli
+# build the server
 cargo build
+# run migration
 diesel run migration
-cd ../home-fm-client
-npm install
+# download fm_transmitter lib
 cd ../fm_transmitter
+# make it
 make
